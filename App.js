115627003718b7c15533254cc533b0bad234c4de
Jens Kuhr Jørgensen <jens@journl.dk>
2020-05-12 10:53:50 +0200

    Create project with two svgs: With and without mask

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View,} from 'react-native';
+
+import {Colors,} from 'react-native/Libraries/NewAppScreen';
+import SvgWithMask from "./SvgWithMask.svg"
+import SvgWithoutMask from "./SvgWithoutMask.svg"
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
+                    <SvgWithMask width={100} height={100}/>
+                    <SvgWithoutMask width={100} height={100}/>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+});
+
+export default App;
