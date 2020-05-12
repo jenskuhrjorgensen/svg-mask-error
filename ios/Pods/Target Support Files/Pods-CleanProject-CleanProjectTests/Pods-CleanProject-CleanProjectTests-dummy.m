9bf0f6c80505df7bfc724b97a06e48387c4a3aab
Jens Kuhr Jørgensen <jens@journl.dk>
2020-05-12 11:18:20 +0200

    Remove caret ^ from package.json. Commit cocoapods.

---
+++ ios/Pods/Target Support Files/Pods-CleanProject-CleanProjectTests/Pods-CleanProject-CleanProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanProject_CleanProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_CleanProject_CleanProjectTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-CleanProject/Pods-CleanProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanProject : NSObject
+@end
+@implementation PodsDummy_Pods_CleanProject
+@end
